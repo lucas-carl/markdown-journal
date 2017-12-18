b578690e24ce8faabf0f52577c50d0387f4a42d4
lucas-carl <mail@lucascarl.com>
2017-12-18 19:00:25 +0100

    update app title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'markdown-journal',
+    title: 'Markdown Journal by Lucas Carl',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
